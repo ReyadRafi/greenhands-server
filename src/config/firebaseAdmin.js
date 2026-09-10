7262fb26baa0016617f6fc376b512b074c5a7af8
Reyad Rafi <reyadrafi009v2@gmail.com>
2026-09-10 18:34:08 +0600

    Refactor Firebase Admin initialization to use modular imports and improve code clarity

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,4 +1,5 @@
-import admin from "firebase-admin";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,8 +7,10 @@ dotenv.config();
 const decodedKey = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
 const serviceAccount = JSON.parse(decodedKey);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+initializeApp({
+  credential: cert(serviceAccount),
 });
+
+const admin = { auth: getAuth };
 
 export default admin;
